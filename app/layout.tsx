3126735acb89743ed9ddc3ed2a357a09825ae647
MohamedFahim123 <https://github.com/MohamedFahim123>
2025-02-20 13:59:35 +0200

    add logo for website

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,14 @@
+import { Metadata } from "next";
 import React from "react";
 import "./globals.css";
+
+export const metadata: Metadata = {
+  title: `DigiFly Task`,
+  description: "DigiFly Task - By Mohamed Fahim (Front End Developer)",
+  icons: {
+    icon: "/logo.svg",
+  },
+};
 
 export default function RootLayout({
   children,
